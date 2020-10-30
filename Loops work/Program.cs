9eaddb1c16957225424d90dfc29d56a2fcef5dd5
BlakeStanley5 <blakestanley542@gmail.com>
2020-10-30 00:50:02 -0500

    loopsWork

---
+++ Loops work/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+
+namespace Loops_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*for (int x = 0; x < 5; x++)
+            {
+                Console.WriteLine("Blake");
+            }
+            */
+
+           /* var dogSizes = new string[] { "big", "small" };
+
+            foreach(var dog in dogSizes)
+            {
+                if(dog == "big")
+                {
+                    Console.WriteLine("Thats a thicc boy!");
+                }
+                else
+                {
+                    Console.WriteLine("that boys not thicc...");
+                }
+           */
+            }
+        }
+    }
+}
